414b8f5d1ec0132c7ec933c7f0bdc28bfaf71e51
Dan Pasanen <dan.pasanen@gmail.com>
2017-08-22 23:02:27 +0300

    x3: replace vendor/cm with vendor/lineage
    Change-Id: I1bdc156a1b8f88f05af8e2b3f8ffaef7a578150f

--- lineage.mk
+++ lineage.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lenovo/x3/msm8992.mk)
