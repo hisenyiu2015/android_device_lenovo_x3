881b3987c0ede67241f80b00911dc7d4d2570c5d
garyyiu2015 <garyyiu2015@gmail.com>
2019-04-27 23:56:52 +0800

    x3: Create firmware symlinks

--- Android.mk
+++ Android.mk
@@ -29,4 +29,88 @@ ifeq ($(TARGET_DEVICE),x3)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+#########################################################################
+# Create WiFi Symlink
+#########################################################################
+$(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/qca_cld)
+
+$(shell ln -s /system/etc/wifi/WCNSS_qcom_cfg.ini $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini)
+$(shell ln -s /system/etc/wifi/WCNSS_qcom_wlan_nv.bin $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/WCNSS_qcom_wlan_nv.bin)
+$(shell ln -s /system/etc/wifi/WCNSS_cfg.dat $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/WCNSS_cfg.dat)
+$(shell ln -s /persist/wlan_mac.bin $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/wlan_mac.bin)
+
+#########################################################################
+# Create IMS Symlink
+#########################################################################
+$(shell mkdir -p $(TARGET_OUT_VENDOR_APPS)/ims/lib/arm64)
+
+$(shell ln -s /system/vendor/lib64/libimscamera_jni.so $(TARGET_OUT_VENDOR_APPS)/ims/lib/arm64/libimscamera_jni.so)
+$(shell ln -s /system/vendor/lib64/libimsmedia_jni.so $(TARGET_OUT_VENDOR_APPS)/ims/lib/arm64/libimsmedia_jni.so)
+
+#########################################################################
+# Create Folder Structure
+#########################################################################
+$(shell rm -rf $(TARGET_OUT)/rfs/)
+
+#To be enabled when prepopulation support is needed for the read_write folder
+# $(shell rm -rf  $(TARGET_OUT_DATA)/rfs/)
+# $(shell mkdir -p $(TARGET_OUT_DATA)/rfs/msm/mpss/)
+# $(shell mkdir -p $(TARGET_OUT_DATA)/rfs/msm/adsp/)
+# $(shell mkdir -p $(TARGET_OUT_DATA)/rfs/mdm/mpss/)
+# $(shell mkdir -p $(TARGET_OUT_DATA)/rfs/mdm/adsp/)
+
+#########################################################################
+# MSM Folders
+#########################################################################
+$(shell mkdir -p $(TARGET_OUT)/rfs/msm/mpss/readonly)
+$(shell mkdir -p $(TARGET_OUT)/rfs/msm/adsp/readonly)
+
+$(shell ln -s /data/tombstones/modem $(TARGET_OUT)/rfs/msm/mpss/ramdumps)
+$(shell ln -s /persist/rfs/msm/mpss $(TARGET_OUT)/rfs/msm/mpss/readwrite)
+$(shell ln -s /persist/rfs/shared $(TARGET_OUT)/rfs/msm/mpss/shared)
+$(shell ln -s /persist/hlos_rfs/shared $(TARGET_OUT)/rfs/msm/mpss/hlos)
+$(shell ln -s /firmware $(TARGET_OUT)/rfs/msm/mpss/readonly/firmware)
+
+$(shell ln -s /data/tombstones/lpass $(TARGET_OUT)/rfs/msm/adsp/ramdumps)
+$(shell ln -s /persist/rfs/msm/adsp $(TARGET_OUT)/rfs/msm/adsp/readwrite)
+$(shell ln -s /persist/rfs/shared $(TARGET_OUT)/rfs/msm/adsp/shared)
+$(shell ln -s /persist/hlos_rfs/shared $(TARGET_OUT)/rfs/msm/adsp/hlos)
+$(shell ln -s /firmware $(TARGET_OUT)/rfs/msm/adsp/readonly/firmware)
+
+#########################################################################
+# MDM Folders
+#########################################################################
+$(shell mkdir -p $(TARGET_OUT)/rfs/mdm/mpss/readonly)
+$(shell mkdir -p $(TARGET_OUT)/rfs/mdm/adsp/readonly)
+$(shell mkdir -p $(TARGET_OUT)/rfs/mdm/sparrow/readonly)
+
+$(shell ln -s /data/tombstones/modem $(TARGET_OUT)/rfs/mdm/mpss/ramdumps)
+$(shell ln -s /persist/rfs/mdm/mpss $(TARGET_OUT)/rfs/mdm/mpss/readwrite)
+$(shell ln -s /persist/rfs/shared $(TARGET_OUT)/rfs/mdm/mpss/shared)
+$(shell ln -s /persist/hlos_rfs/shared $(TARGET_OUT)/rfs/mdm/mpss/hlos)
+$(shell ln -s /firmware $(TARGET_OUT)/rfs/mdm/mpss/readonly/firmware)
+
+$(shell ln -s /data/tombstones/lpass $(TARGET_OUT)/rfs/mdm/adsp/ramdumps)
+$(shell ln -s /persist/rfs/mdm/adsp $(TARGET_OUT)/rfs/mdm/adsp/readwrite)
+$(shell ln -s /persist/rfs/shared $(TARGET_OUT)/rfs/mdm/adsp/shared)
+$(shell ln -s /persist/hlos_rfs/shared $(TARGET_OUT)/rfs/mdm/adsp/hlos)
+$(shell ln -s /firmware $(TARGET_OUT)/rfs/mdm/adsp/readonly/firmware)
+
+$(shell ln -s /data/tombstones/sparrow $(TARGET_OUT)/rfs/mdm/sparrow/ramdumps)
+$(shell ln -s /persist/rfs/mdm/sparrow $(TARGET_OUT)/rfs/mdm/sparrow/readwrite)
+$(shell ln -s /persist/rfs/shared $(TARGET_OUT)/rfs/mdm/sparrow/shared)
+$(shell ln -s /persist/hlos_rfs/shared $(TARGET_OUT)/rfs/mdm/sparrow/hlos)
+$(shell ln -s /firmware $(TARGET_OUT)/rfs/mdm/sparrow/readonly/firmware)
+
+#########################################################################
+# APQ Folders
+#########################################################################
+$(shell mkdir -p $(TARGET_OUT)/rfs/apq/gnss/readonly)
+
+$(shell ln -s /data/tombstones/modem $(TARGET_OUT)/rfs/apq/gnss/ramdumps)
+$(shell ln -s /persist/rfs/apq/gnss $(TARGET_OUT)/rfs/apq/gnss/readwrite)
+$(shell ln -s /persist/rfs/shared $(TARGET_OUT)/rfs/apq/gnss/shared)
+$(shell ln -s /persist/hlos_rfs/shared $(TARGET_OUT)/rfs/apq/gnss/hlos)
+$(shell ln -s /firmware $(TARGET_OUT)/rfs/apq/gnss/readonly/firmware)
+
 endif
